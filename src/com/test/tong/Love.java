ec3646d71a4b3b5459ad81440cce82713aaaa7f9
may222 <may@may-PC>
2016-05-05 11:29:04 +0800

    this

--- src/com/test/tong/Love.java
+++ src/com/test/tong/Love.java
@@ -4,7 +4,7 @@ public class Love {
 
 	public static void main(String[] args) {
 		
-		System.out.println("i love you for real...");
+		System.out.println("i love you for real... it is true ");
 	}
 
 }
